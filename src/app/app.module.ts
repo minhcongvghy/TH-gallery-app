72c6f81b285540f0750c2682e9a74e7de6022540
Cong <minhcongvghy@gmail.com>
2019-10-25 10:31:41 +0700

    [Thực hành] Ứng dụng Gallery

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+import { ImageCardComponent } from './image-gallery/image-card/image-card.component';
+import {GalleryConfig} from './image-gallery/token';
+import {ImageGalleryModule} from './image-gallery/image-gallery.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ImageGalleryModule
+  ],
+  providers: [
+    {provide: GalleryConfig, useValue: 3}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
